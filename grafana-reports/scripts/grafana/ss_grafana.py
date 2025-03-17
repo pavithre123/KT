6bec05e48345f9473b08772c571113bfda624963
Pavithra Rathnayaka <pavithra.rathnayake@axiatadigitallabs.com>
2025-03-17 11:53:18 +0530

    add seperate cronjobs

--- grafana-reports/scripts/grafana/ss_grafana.py
+++ grafana-reports/scripts/grafana/ss_grafana.py
@@ -8,49 +8,51 @@ import time
 
 # Setup Chrome driver with headless option
 # service = Service(ChromeDriverManager().install())
-# service = Service("/grafana-reports/chromedriver-linux64/chromedriver")
-# chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument("--headless")
-# chrome_options.add_argument("--window-size=1920,5000")  # Set initial window size
-# chrome_options.add_argument("--no-sandbox")
-# chrome_options.add_argument("--disable-dev-shm-usage")
-# chrome_options.add_argument("--disable-gpu")
+service = Service("/grafana-reports/chromedriver-linux64/chromedriver")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920,5000")  # Set initial window size
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
 
-# driver = webdriver.Chrome(service=service, options=chrome_options)
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
-urls = ["http://axonect-monetiser-grafana/d/3X-EcUm4ka/axp-gateway-traffic-dashboard-pdfa?orgId=1&from=now-12h&to=now","http://grafana.axp.com/d/3X-EcUm4ka/axp-gateway-traffic-dashboard-pdfa?orgId=1&from=now-12h&to=now","http://grafana.axp.com/d/3X-EcUmasdf/axp-gateway-traffic-dashboar?orgId=1&from=now-12h&to=now","http://grafana.axp.com/d/3X-Ehrhkgk4ka/axp-gateway-traffic-dashboard-pdf?orgId=1&from=now-12h&to=now","http://grafana.axp.com/d/3X-pwlsnnm4ka/axp-gateway-traffic?orgId=1&from=now-12h&to=now"]
+urls = ["http://axonect-monetiser-grafana/d/3X-EcUm4ka/axp-gateway-traffic-dashboard-pdfa?orgId=1&from=now-12h&to=now",
+        "http://grafana.axp.com/d/3X-EcUm4ka/axp-gateway-traffic-dashboard-pdfa?orgId=1&from=now-12h&to=now",
+        "http://grafana.axp.com/d/3X-EcUmasdf/axp-gateway-traffic-dashboar?orgId=1&from=now-12h&to=now",
+        "http://grafana.axp.com/d/3X-Ehrhkgk4ka/axp-gateway-traffic-dashboard-pdf?orgId=1&from=now-12h&to=now",
+        "http://grafana.axp.com/d/3X-pwlsnnm4ka/axp-gateway-traffic?orgId=1&from=now-12h&to=now"]
 
-print(urls[0])
+try:
+    driver.get("http://grafana.axp.com/d/3X-EcUm4ka/axp-gateway-traffic-dashboard-pdfa?orgId=1&from=now-12h&to=now")
 
-# try:
-#     driver.get("http://grafana.axp.com/d/3X-EcUm4ka/axp-gateway-traffic-dashboard-pdfa?orgId=1&from=now-12h&to=now")
+    # Wait for the login fields to be present
+    username_field = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.NAME, 'user'))
+    )
+    password_field = driver.find_element(By.NAME, 'password')
 
-#     # Wait for the login fields to be present
-#     username_field = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.NAME, 'user'))
-#     )
-#     password_field = driver.find_element(By.NAME, 'password')
+    # Enter credentials
+    username_field.send_keys("admin")
+    password_field.send_keys("admin")
 
-#     # Enter credentials
-#     username_field.send_keys("admin")
-#     password_field.send_keys("admin")
+    # Submit the form
+    login_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
+    login_button.click()
 
-#     # Submit the form
-#     login_button = driver.find_element(By.XPATH, '//button[@type="submit"]')
-#     login_button.click()
+    # Wait for the dashboard to load
+    time.sleep(10)
 
-#     # Wait for the dashboard to load
-#     time.sleep(10)
+    # print(total_height)
+    driver.set_window_size(1920, 5000)
+    print("All dashboards are loaded with data.")
 
-#     # print(total_height)
-#     driver.set_window_size(1920, 5000)
-#     print("All dashboards are loaded with data.")
+    # Take a screenshot of the entire page
+    dashboard_area = driver.find_element(By.XPATH, '//*[@class="react-grid-layout"]')
+    # driver.save_screenshot("full_ss.png")
+    dashboard_area.screenshot("dashboard_area.png")
+    print("Screenshot Created!!!")
 
-#     # Take a screenshot of the entire page
-#     dashboard_area = driver.find_element(By.XPATH, '//*[@class="react-grid-layout"]')
-#     # driver.save_screenshot("full_ss.png")
-#     dashboard_area.screenshot("dashboard_area.png")
-#     print("Screenshot Created!!!")
-
-# finally:
-#     driver.quit()
+finally:
+    driver.quit()
